8f534e79e9e27149363f937c1b9534afc25bff2f
Jason Lethbridge <lethbridgejason@gmail.com>
2023-05-17 04:06:53 +1000

    Added some basic controls

--- src/gui/win32/main.c
+++ src/gui/win32/main.c
@@ -1,70 +1,111 @@
 #if defined(UNICODE) && !defined(_UNICODE)
-    #define _UNICODE
+#define _UNICODE
 #elif defined(_UNICODE) && !defined(UNICODE)
-    #define UNICODE
+#define UNICODE
 #endif
 
 #include <tchar.h>
 #include <windows.h>
 
-/*  Declare Windows procedure  */
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+/* TODO: Add a manifest to enable visual styles */
+
+typedef struct NewThInterface {
+    HWND winMain;
+    HWND btnRun;
+    HWND btnBrowse;
+    HWND entryPath;
+    HWND labelPath;
+    HWND listAdapter;
+    HWND listConnections;
+} NewThInterface;
+
+#define NORMAL_WIDGET (WS_VISIBLE | WS_CHILD)
+
+#define NORMAL_BUTTON (NORMAL_WIDGET | BS_DEFPUSHBUTTON | WS_TABSTOP)
+
+#define NORMAL_LABEL (NORMAL_WIDGET | SS_CENTERIMAGE)
+
+#define NORMAL_ENTRY (NORMAL_WIDGET | WS_BORDER)
 
 /*  Make the class name into a global variable  */
-TCHAR szClassName[ ] = _T("NewThInterface");
+TCHAR szClassName[] = _T("NewThInterface");
 
-int WINAPI WinMain (HINSTANCE hThisInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR lpszArgument,
-                     int nCmdShow)
-{
-    HWND hwnd;               /* This is the handle for our window */
-    MSG messages;            /* Here messages to the application are saved */
-    WNDCLASSEX wincl;        /* Data structure for the window class */
+NewThInterface NewThCreate(HINSTANCE inst, int show) {
+    NewThInterface r;
 
-    /* The Window structure */
-    wincl.hInstance = hThisInstance;
-    wincl.lpszClassName = szClassName;
-    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
-    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
-    wincl.cbSize = sizeof (WNDCLASSEX);
+    /* Create the desktop Window */
+    r.winMain = CreateWindow (szClassName,              /* Class name */
+                              _T("Newth Http Server"),  /* Title Text */
+                              WS_OVERLAPPEDWINDOW,      /* default window */
+                              CW_USEDEFAULT,            /* Windows decides the position */
+                              CW_USEDEFAULT,            /* where the window ends up on the screen */
+                              320,                      /* The programs width */
+                              240,                      /* and height in pixels */
+                              HWND_DESKTOP,             /* The window is a child-window to desktop */
+                              NULL,                     /* No menu */
+                              inst,                     /* Program Instance handler */
+                              NULL                      /* No Window Creation data */
+    );
 
-    /* Use default icon and mouse-pointer */
-    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-    wincl.lpszMenuName = NULL;                 /* No menu */
-    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-    wincl.cbWndExtra = 0;                      /* structure or the window instance */
-    /* Use Windows's default color as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+    /* Create the Start/Stop server button */
+    r.btnRun = CreateWindow (_T("BUTTON"), _T("Start Server"), NORMAL_BUTTON, 10, 45, 100, 25, r.winMain, 0, inst, 0);
 
-    /* Register the window class, and if it fails quit the program */
-    if (!RegisterClassEx (&wincl))
-        return 0;
-
-    /* The class is registered, let's create the program*/
-    hwnd = CreateWindowEx (
-           0,                       /* Extended possibilities for variation */
-           szClassName,             /* Class name */
-           _T("Newth Http Server"), /* Title Text */
-           WS_OVERLAPPEDWINDOW,     /* default window */
-           CW_USEDEFAULT,           /* Windows decides the position */
-           CW_USEDEFAULT,           /* where the window ends up on the screen */
-           320,                     /* The programs width */
-           240,                     /* and height in pixels */
-           HWND_DESKTOP,            /* The window is a child-window to desktop */
-           NULL,                    /* No menu */
-           hThisInstance,           /* Program Instance handler */
-           NULL                     /* No Window Creation data */
-           );
+    /* Create the path label, entry & button widgets */
+    r.labelPath = CreateWindow (_T("STATIC"), _T("Path:"), NORMAL_LABEL, 10, 10, 40, 25, r.winMain, 0, inst, 0);
+    r.entryPath = CreateWindow (_T("EDIT"), _T(""), NORMAL_ENTRY, 60, 10, 170, 25, r.winMain, 0, inst, 0);
+    r.btnBrowse = CreateWindow (_T("BUTTON"), _T("Browse..."), NORMAL_BUTTON, 240, 10, 70, 25, r.winMain, 0, inst, 0);
 
     /* Make the window visible on the screen */
-    ShowWindow (hwnd, nCmdShow);
+    ShowWindow(r.winMain, show);
+
+    return r;
+}
+
+/*  This function is called by the Windows function DispatchMessage()  */
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_DESTROY:
+            PostQuitMessage(0); /* send a WM_QUIT to the message queue */
+            break;
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow) {
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX winClass;        /* Data structure for the window class */
+    NewThInterface newTh;
+
+    /* The Window structure */
+    winClass.hInstance = hThisInstance;
+    winClass.lpszClassName = szClassName;
+    winClass.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    winClass.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    winClass.cbSize = sizeof(WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    winClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    winClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    winClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    winClass.lpszMenuName = NULL; /* No menu */
+    winClass.cbClsExtra = 0;      /* No extra bytes after the window class */
+    winClass.cbWndExtra = 0;      /* structure or the window instance */
+
+    /* Use Windows's default color as the background of the window */
+    winClass.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx(&winClass))
+        return 0;
+
+    /* The class is registered, create the program */
+    newTh = NewThCreate(hThisInstance, nCmdShow);
 
     /* Run the message loop. It will run until GetMessage() returns 0 */
-    while (GetMessage (&messages, NULL, 0, 0))
-    {
+    while (GetMessage(&messages, NULL, 0, 0)) {
         /* Translate virtual-key messages into character messages */
         TranslateMessage(&messages);
         /* Send message to WindowProcedure */
@@ -73,21 +114,4 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 
     /* The program return-value is 0 - The value that PostQuitMessage() gave */
     return messages.wParam;
-}
-
-
-/*  This function is called by the Windows function DispatchMessage()  */
-
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)                /* handle the messages */
-    {
-        case WM_DESTROY:
-            PostQuitMessage (0);    /* send a WM_QUIT to the message queue */
-            break;
-        default:                    /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
-    }
-
-    return 0;
 }
