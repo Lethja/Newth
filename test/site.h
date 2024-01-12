9dc4bcbc2c4993c30177ace5bff997a210aad257
Jason Lethbridge <lethbridgejason@gmail.com>
2024-01-13 10:11:05 +1100

    Added http site unit tests

--- test/site.h
+++ test/site.h
@@ -17,6 +17,8 @@
 
 #pragma endregion
 
+#pragma region Site Array Tests
+
 static void SiteArrayFunctions(void **state) {
     Site site1, site2;
     memset(&site1, 0, sizeof(Site)), memset(&site2, 0, sizeof(Site));
@@ -51,6 +53,10 @@ static void SiteArrayFunctions(void **state) {
     assert_int_equal(siteArrayGetActiveNth(), -1);
     siteArrayFree(), siteFree(&site1);
 }
+
+#pragma endregion
+
+#pragma region File Site Tests
 
 static void SiteFileNew(void **state) {
     Site site;
@@ -124,10 +130,133 @@ static void SiteFileDirEntry(void **state) {
     siteCloseDirectoryListing(&site, d), siteFree(&site);
 }
 
+#pragma endregion
+
+#pragma region Http Site Tests
+
+#ifdef MOCK
+
+const char *HttpHeaderResponseDir = "HTTP/1.1 200 OK" HTTP_EOL
+                                    "Content-Type: text/html; charset=ISO-8859-1" HTTP_EOL
+                                    "Date: Thu, 1 Jan 1970 00:00:00 GMT" HTTP_EOL HTTP_EOL;
+
+const char *HttpHeaderResponseFile = "HTTP/1.1 200 OK" HTTP_EOL
+                                     "Content-Type: text/html; charset=ISO-8859-1" HTTP_EOL
+                                     "Content-Disposition: attachment"
+                                     "Date: Thu, 1 Jan 1970 00:00:00 GMT" HTTP_EOL HTTP_EOL;
+
+static void SiteHttpNew(void **state) {
+    Site site;
+    assert_string_equal(siteNew(&site, SITE_HTTP, NULL), "No uri specified");
+    assert_int_equal(site.type, SITE_HTTP);
+    assert_int_equal(site.site.http.socket, -1);
+    assert_null(site.site.http.fullUri);
+    siteFree(&site);
+}
+
+static void SiteHttpNewWithPath(void **state) {
+    Site site;
+
+    mockReset(), mockOptions = MOCK_CONNECT | MOCK_SEND | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
+    mockReceiveStream = tmpfile(), fwrite(HttpHeaderResponseDir, 1, strlen(HttpHeaderResponseDir),
+                                          mockReceiveStream), rewind(mockReceiveStream);
+
+    assert_null(siteNew(&site, SITE_HTTP, "http://127.0.0.1"));
+    assert_int_equal(site.type, SITE_HTTP);
+    assert_non_null(site.site.http.fullUri);
+    assert_string_equal(site.site.http.fullUri, "http://127.0.0.1/"); /* Note that '/' has been appended at the end */
+    siteFree(&site);
+}
+
+static void SiteHttpGetDirectory(void **state) {
+    Site site;
+    UriDetails details;
+    char *wd;
+
+    mockReset(), mockOptions = MOCK_CONNECT | MOCK_SEND | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
+    mockReceiveStream = tmpfile(), fwrite(HttpHeaderResponseDir, 1, strlen(HttpHeaderResponseDir),
+                                          mockReceiveStream), rewind(mockReceiveStream);
+
+    assert_null(siteNew(&site, SITE_HTTP, "http://127.0.0.1"));
+    assert_non_null(site.site.http.fullUri);
+
+    wd = siteGetWorkingDirectory(&site);
+    assert_string_equal(wd, "http://127.0.0.1/");
+
+    details = uriDetailsNewFrom(wd);
+    assert_string_equal(details.host, "127.0.0.1");
+    assert_string_equal(details.path, "/");
+
+    uriDetailsFree(&details), siteFree(&site);
+}
+
+static void SiteHttpSetDirectory(void **state) {
+    Site site;
+    char *wd = "http://127.0.0.1/foo";
+
+    mockReset(), mockOptions = MOCK_CONNECT | MOCK_SEND | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
+    mockReceiveStream = tmpfile(), fwrite(HttpHeaderResponseDir, 1, strlen(HttpHeaderResponseDir),
+                                          mockReceiveStream), rewind(mockReceiveStream);
+
+    assert_null(siteNew(&site, SITE_HTTP, wd)), rewind(mockReceiveStream);
+    assert_non_null(site.site.file.fullUri);
+    assert_string_equal(siteGetWorkingDirectory(&site), wd);
+
+    siteSetWorkingDirectory(&site, ".."), rewind(mockReceiveStream);
+    assert_string_equal(siteGetWorkingDirectory(&site), "http://127.0.0.1/");
+
+    siteSetWorkingDirectory(&site, "foo"), rewind(mockReceiveStream);
+    assert_string_equal(siteGetWorkingDirectory(&site), wd);
+
+    siteSetWorkingDirectory(&site, "bar"), rewind(mockReceiveStream);
+    assert_string_equal(siteGetWorkingDirectory(&site), "http://127.0.0.1/foo/bar");
+
+    siteSetWorkingDirectory(&site, "/bar");
+    assert_string_equal(siteGetWorkingDirectory(&site), "http://127.0.0.1/bar");
+
+    siteFree(&site);
+}
+
+static void SiteHttpDirEntry(void **state) {
+    Site site;
+    size_t i, j = i = 0;
+    void *d, *e;
+
+    mockReset(), mockOptions = MOCK_CONNECT | MOCK_SEND | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
+
+    assert_null(siteNew(&site, SITE_HTTP, "http://127.0.0.1"));
+    d = platformDirOpen(".");
+    assert_non_null(d);
+
+    while ((e = platformDirRead(d))) {
+        if (platformDirEntryGetName(e, NULL)[0] != '.') /* Exclude all names starting with '.' */
+            ++i;
+    }
+
+    platformDirClose(d), d = siteOpenDirectoryListing(&site, ".");
+    assert_non_null(d);
+
+    while ((e = siteReadDirectoryListing(&site, d)))
+        ++j, siteDirectoryEntryFree(e);
+
+    assert_int_equal(i, j);
+
+    siteCloseDirectoryListing(&site, d), siteFree(&site);
+}
+
+#endif /* MOCK */
+
+#pragma endregion
+
 #pragma clang diagnostic pop
 
 const struct CMUnitTest siteTest[] = {cmocka_unit_test(SiteArrayFunctions), cmocka_unit_test(SiteFileNew),
                                       cmocka_unit_test(SiteFileNewWithPath), cmocka_unit_test(SiteFileGetDirectory),
-                                      cmocka_unit_test(SiteFileSetDirectory), cmocka_unit_test(SiteFileDirEntry)};
+                                      cmocka_unit_test(SiteFileSetDirectory), cmocka_unit_test(SiteFileDirEntry)
+#ifdef MOCK
+        , cmocka_unit_test(SiteHttpNew), cmocka_unit_test(SiteHttpNewWithPath), cmocka_unit_test(SiteHttpGetDirectory),
+                                      cmocka_unit_test(SiteHttpSetDirectory), /*cmocka_unit_test(SiteHttpDirEntry)*/
+#endif
+};
 
 #endif /* NEW_DL_TEST_SITE_H */
