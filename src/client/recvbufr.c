13215525a37f805254c78efa73ef2e99807d42d0
Jason Lethbridge <lethbridgejason@gmail.com>
2024-02-11 15:27:43 +1100

    Added initial recvbufr code

---
+++ src/client/recvbufr.c
@@ -0,0 +1,116 @@
+#include "recvbufr.h"
+
+char *recvBufferAppend(RecvBuffer *self, size_t len) {
+    size_t i = 0;
+
+    if (!self->buffer)
+        self->buffer = platformMemoryStreamNew();
+    else
+        platformMemoryStreamSeek(self->buffer, SEEK_END, 0);
+
+    while (i < len) {
+        char buf[SB_DATA_SIZE];
+        PlatformFileOffset l;
+
+        switch ((l = recv(self->serverSocket, buf, SB_DATA_SIZE, 0))) {
+            case -1:
+                /* TODO: Handle when something is wrong with the socket */
+                return strerror(platformSocketGetLastError());
+            case 0:
+                return "No data to be retrieved";
+            default:
+                /* TODO: Get correct error to return */
+                if ((fwrite(buf, 1, l, self->buffer)) != l)
+                    return strerror(errno);
+
+                self->remain -= l, self->escape += l;
+                i += l;
+                break;
+        }
+    }
+
+    return NULL;
+}
+
+void recvBufferClear(RecvBuffer *self) {
+    if (self->buffer)
+        fclose(self->buffer), self->buffer = NULL;
+}
+
+void recvBufferDitch(RecvBuffer *self, PlatformFileOffset len) {
+    FILE *tmp;
+    SOCK_BUF_TYPE i;
+    char buf[SB_DATA_SIZE];
+
+    if (!self->buffer || !(tmp = tmpfile()))
+        return;
+
+    platformMemoryStreamSeek(self->buffer, len, SEEK_SET);
+    while ((i = fread(buf, 1, SB_DATA_SIZE, self->buffer)))
+        fwrite(buf, 1, i, tmp);
+
+    fclose(self->buffer), self->buffer = tmp;
+}
+
+void recvBufferFailFree(RecvBuffer *self) {
+    if (self->buffer)
+        fclose(self->buffer);
+}
+
+PlatformFileOffset recvBufferFind(RecvBuffer *self, PlatformFileOffset pos, const char *data, size_t len) {
+    PlatformFileOffset r;
+    size_t i, j, l;
+    char buf[SB_DATA_SIZE];
+
+    if (!self->buffer)
+        return -1;
+
+    j = r = 0, platformMemoryStreamSeek(self->buffer, pos, SEEK_SET);
+    while ((l = fread(buf, 1, SB_DATA_SIZE, self->buffer)) > 0) {
+        for (i = 0; i < l; ++i) {
+            if (buf[i] == data[j]) {
+                ++j;
+                if (j == len) {
+                    r += (PlatformFileOffset) j;
+                    return r;
+                }
+            } else
+                j = 0;
+        }
+
+        r += (PlatformFileOffset) l;
+    }
+
+    return -1;
+}
+
+const char *recvBufferSearchFor(RecvBuffer *self, const char *token, size_t len) {
+    PlatformFileOffset o = 0;
+    const char *e;
+
+    while (!(e = recvBufferAppend(self, SB_DATA_SIZE))) {
+        if ((o = recvBufferFind(self, 0, token, len)) == -1) {
+            PlatformFileOffset p;
+
+            platformMemoryStreamSeek(self->buffer, 0, SEEK_END), p = platformMemoryStreamTell(self->buffer);
+            if (p > len)
+                p -= (PlatformFileOffset) len, recvBufferDitch(self, p);
+        } else {
+            if (o > 1)
+                recvBufferDitch(self, o);
+
+            return NULL;
+        }
+    }
+
+    return e;
+}
+
+RecvBuffer recvBufferNew(SOCKET serverSocket, char options) {
+    /* TODO: Make aware of site and/or socketAddress for reconnection */
+    RecvBuffer self;
+
+    self.options = options, self.serverSocket = serverSocket;
+    self.escape = self.remain = 0, self.buffer = NULL;
+    return self;
+}
---
+++ src/client/recvbufr.h
@@ -0,0 +1,58 @@
+#ifndef NEW_DL_RECEIVE_BUFFER_H
+#define NEW_DL_RECEIVE_BUFFER_H
+
+#include "../platform/platform.h"
+#include "site.h"
+
+typedef struct RecvBuffer {
+    PlatformFileOffset escape, remain;
+    FILE *buffer;
+    SOCKET serverSocket;
+    char options;
+} RecvBuffer;
+
+/**
+ * Read data out of the socket and append to the buffer
+ * @param self The buffer to start or append more network data into the buffer of
+ * @param len The length of the network data to fetch
+ * @return
+ */
+char *recvBufferAppend(RecvBuffer *self, size_t len);
+
+/**
+ * Remove the buffer from memory if there is one
+ * @param self The buffer to clear of memory allocations
+ */
+void recvBufferClear(RecvBuffer *self);
+
+/**
+ * Find a specific character
+ * @param self In: The buffer to find data in
+ * @param pos In: The position from the start of the temporary buffer to look
+ * @param data In: The data to match
+ * @param len In: The length of the data to match
+ * @return The position relative to the start of the buffer or -1 when no match is found
+ */
+PlatformFileOffset recvBufferFind(RecvBuffer *self, PlatformFileOffset pos, const char *data, size_t len);
+
+/**
+ * Disregard a certain amount of the start of the current buffer
+ * @param self The buffer to disregard the start of
+ * @param len The amount of data from the start to disregard
+ */
+void recvBufferDitch(RecvBuffer *self, PlatformFileOffset len);
+
+RecvBuffer recvBufferNew(SOCKET serverSocket, char options);
+
+/**
+ * Search for a byte match to data in the sockets data consuming everything in the stream before the match
+ * @param self In: The buffer to search for the token in
+ * @param token In: The data to match
+ * @param len In: The length of the data to match
+ * @return NULL on match, user friendly error message otherwise
+ * @remark The buffer will be consumed up to the first matched instance,
+ * if no match is found the entire buffer will be consumed! Use this function with caution.
+ */
+const char *recvBufferSearchFor(RecvBuffer *self, const char *token, size_t len);
+
+#endif /* NEW_DL_RECEIVE_BUFFER_H */
