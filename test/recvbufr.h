5f972df792a4b84fceaa012de8a31756d390457d
Jason Lethbridge <lethbridgejason@gmail.com>
2024-02-21 01:44:09 +1100

    Improve RecvBufferClear unit test

--- test/recvbufr.h
+++ test/recvbufr.h
@@ -37,6 +37,7 @@ static void RecvBufferMemoryFree(void **state) {
 
 static void RecvBufferClear(void **state) {
     RecvBuffer socketBuffer = recvBufferNew(0, 0);
+    char buf[10];
 
 #ifdef MOCK
     FILE *buffer;
@@ -44,9 +45,11 @@ static void RecvBufferClear(void **state) {
     buffer =
     #endif
 
-            socketBuffer.buffer = platformMemoryStreamNew();
+    socketBuffer.buffer = platformMemoryStreamNew();
     assert_non_null(&socketBuffer.buffer);
     recvBufferClear(&socketBuffer);
+    assert_null(socketBuffer.buffer);
+    assert_non_null(recvBufferFetch(&socketBuffer, buf, 0, 10));
 
 #ifdef MOCK
     assert_ptr_equal(buffer, mockLastFileClosed);
@@ -54,27 +57,6 @@ static void RecvBufferClear(void **state) {
 }
 
 #ifdef MOCK
-
-static void ReceiveFetch(void **state) {
-    RecvBuffer socketBuffer;
-    char buf[11] = {0};
-
-    mockReset(), mockOptions = MOCK_CONNECT | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
-
-    socketBuffer = recvBufferNew(0, 0);
-    assert_null(recvBufferAppend(&socketBuffer, 10));
-
-    assert_null(recvBufferFetch(&socketBuffer, buf, 0, 11));
-    assert_string_equal("1234567890", buf);
-
-    assert_null(recvBufferFetch(&socketBuffer, buf, 1, 5));
-    assert_string_equal("2345", buf);
-
-    assert_null(recvBufferFetch(&socketBuffer, buf, 5, 4));
-    assert_string_equal("678", buf);
-
-    recvBufferFailFree(&socketBuffer);
-}
 
 static void ReceiveDitch(void **state) {
     RecvBuffer socketBuffer;
@@ -96,6 +78,27 @@ static void ReceiveDitch(void **state) {
     recvBufferDitch(&socketBuffer, 0); /* Don't ditch if zero */
     assert_null(recvBufferFetch(&socketBuffer, buf, 0, 11));
     assert_string_equal("7890", buf);
+
+    recvBufferFailFree(&socketBuffer);
+}
+
+static void ReceiveFetch(void **state) {
+    RecvBuffer socketBuffer;
+    char buf[11] = {0};
+
+    mockReset(), mockOptions = MOCK_CONNECT | MOCK_RECEIVE, mockSendMaxBuf = mockReceiveMaxBuf = 1024;
+
+    socketBuffer = recvBufferNew(0, 0);
+    assert_null(recvBufferAppend(&socketBuffer, 10));
+
+    assert_null(recvBufferFetch(&socketBuffer, buf, 0, 11));
+    assert_string_equal("1234567890", buf);
+
+    assert_null(recvBufferFetch(&socketBuffer, buf, 1, 5));
+    assert_string_equal("2345", buf);
+
+    assert_null(recvBufferFetch(&socketBuffer, buf, 5, 4));
+    assert_string_equal("678", buf);
 
     recvBufferFailFree(&socketBuffer);
 }
